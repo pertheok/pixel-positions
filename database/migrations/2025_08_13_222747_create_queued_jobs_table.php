b1883441cc22de8c30c5ea6778c0639d4937f0bd
Jakub Slawecki <kubaslawecki@gmail.com>
2025-08-18 01:00:44 +0100

    Update migrations and queue driver

---
+++ database/migrations/2025_08_13_222747_create_queued_jobs_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('queued_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('queue')->index();
+            $table->longText('payload');
+            $table->unsignedTinyInteger('attempts');
+            $table->unsignedInteger('reserved_at')->nullable();
+            $table->unsignedInteger('available_at')->nullable();
+            $table->unsignedInteger('created_at')->nullable();
+        });
+
+        Schema::create('queued_job_batches', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('total_jobs');
+            $table->integer('pending_jobs');
+            $table->integer('failed_jobs');
+            $table->longText('failed_job_ids');
+            $table->mediumText('options')->nullable();
+            $table->integer('cancelled_at')->nullable();
+            $table->integer('created_at');
+            $table->integer('finished_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('queued_jobs');
+        Schema::dropIfExists('queued_job_batches');
+    }
+};
