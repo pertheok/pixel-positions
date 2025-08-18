5bdce8d1db9669c25eb2cb640071d1f597024403
Jakub Slawecki <kubaslawecki@gmail.com>
2025-08-18 01:02:18 +0100

    Initialise model files

---
+++ app/Http/Controllers/JobsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Jobs;
+use App\Http\Requests\StoreJobsRequest;
+use App\Http\Requests\UpdateJobsRequest;
+
+class JobsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreJobsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Jobs $jobs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Jobs $jobs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateJobsRequest $request, Jobs $jobs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Jobs $jobs)
+    {
+        //
+    }
+}
