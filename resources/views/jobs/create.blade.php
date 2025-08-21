b7fa257318c0343aa313d9a092ffc39137352e4f
Jakub Slawecki <kubaslawecki@gmail.com>
2025-08-21 19:50:41 +0100

    Allow for creation of jobs

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -2,16 +2,19 @@
     <x-page-heading>New Job</x-page-heading>
 
     <x-forms.form method="POST" action="/jobs">
-        <x-forms.input label="Title" name="title" />
-        <x-forms.input label="Salary" name="salary" />
-        <x-forms.input label="Location" name="location" />
+        <x-forms.input label="Title" name="title" placeholder="CEO" />
+        <x-forms.input label="Salary" name="salary" placeholder="$50,000 USD" />
+        <x-forms.input label="Location" name="location" placeholder="Stambul" />
 
         <x-forms.select label="Schedule" name="schedule">
             <option>Part Time</option>
             <option>Full Time</option>
         </x-forms.select>
 
-        <x-forms.input label="URL" name="url" placeholder="https://www.twitch.tv/masondota2" />
+        <x-forms.input label="URL" name="url" placeholder="https://www.somecompany.com" />
+        <x-forms.checkbox label="Feature (Costs Extra)" name="featured" />
+
+        <x-forms.divider />
 
         <x-forms.input label="Tags (Comma separated)" name="tags" placeholder="portfolio, video, education" />
 
