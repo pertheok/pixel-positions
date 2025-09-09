47726fbb7d93c9c237f25b37c1a58aa45febb8ed
Jakub Slawecki <kubaslawecki@gmail.com>
2025-09-09 23:13:11 +0100

    Implement tokens

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('tickets', TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
