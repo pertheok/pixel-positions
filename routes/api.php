a46963cd6a94b1bb1d0a41ddafbb496d20244750
Jakub Slawecki <kubaslawecki@gmail.com>
2025-09-15 22:42:21 +0100

    Conditionally omit return data

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
