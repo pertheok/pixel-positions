1dbe868cabbcd4fcf0a6cca0c7431439a57d8ad0
Jakub Slawecki <kubaslawecki@gmail.com>
2025-09-18 23:26:52 +0100

    Implement creation

--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -7,6 +7,8 @@ use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Requests\Api\V1\UpdateTicketRequest;
 use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
+use App\Models\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class TicketController extends ApiController
 {
@@ -28,7 +30,26 @@ class TicketController extends ApiController
      */
     public function store(StoreTicketRequest $request)
     {
-        //
+        /**
+         * Below is a custom validation for those who don't want to use the Request validation. 
+         * Returns 200 to obscure the existence of the error from automated attacks analysing response codes.
+         */
+        // try {
+        //     $user = User::findOrFail($request->input('data.relationships.author.data.id'));
+        // } catch (ModelNotFoundException $e) {
+        //     return $this->ok('User not found', [
+        //         'error' => 'The specified author ID does not exist.',
+        //     ]);
+        // }
+
+        $model = [
+            'title' => $request->input('data.attributes.title'),
+            'description' => $request->input('data.attributes.description'),
+            'status' => $request->input('data.attributes.status'),
+            'user_id' => $request->input('data.relationships.author.data.id'),
+        ];
+
+        return new TicketResource(Ticket::create($model));
     }
 
     /**
--- app/Http/Requests/Api/V1/StoreTicketRequest.php
+++ app/Http/Requests/Api/V1/StoreTicketRequest.php
@@ -11,7 +11,7 @@ class StoreTicketRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,8 +21,24 @@ class StoreTicketRequest extends FormRequest
      */
     public function rules(): array
     {
+        $rules = [
+            'data.attributes.title' => ['required', 'string'],
+            'data.attributes.description' => ['required', 'string'],
+            'data.attributes.status' => ['required', 'string', 'in:A,C,H,X'],
+        ];
+
+        if ($this->routeIs('tickets.store')) {
+            $rules['data.relationships.author.data.id'] = ['required', 'integer', 'exists:users,id'];
+        }
+
+        return $rules;
+    }
+
+    public function messages() 
+    {
         return [
-            //
+            'data.attributes.status.in' => 'The status field must be one of the following values: A, C, H, X.',
+            'data.relationships.author.data.id.exists' => 'The selected author does not exist.',
         ];
     }
 }
